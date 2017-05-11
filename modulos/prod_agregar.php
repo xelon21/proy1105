03b2a5bd31ef253d8e89d747e5854f879fb1cf00
cetecom <cetecom@LC08L1222>
2017-05-11 13:12:57 -0300

    clase 11-05-2017 2

---
+++ modulos/prod_agregar.php
@@ -0,0 +1,16 @@
+<?php
+include '../lib/conexion.php';
+$conn=new conexion;
+
+if ($conn->conectar()){
+$nom=$_POST["nombre"];
+$totusd=$_POST["totalusd"];
+$ano=$_POST["ano"];
+
+$sql="INSERT INTO productos(nombre, totalusd, ano) values('$nom', $totusd, $ano)";
+
+ echo $sql;
+ 
+}
+         
+    
